4ded5000f92fd5b1e46009b2a4ee26123a896694
Mark Vogt <markevogt@outlook.com>
2021-02-27 18:18:43 -0600

    ADDED customFunctions.py... MODIFIED init...

--- mvAzureFunction01/__init__.py
+++ mvAzureFunction01/__init__.py
@@ -2,11 +2,24 @@ import logging
 
 import azure.functions as func
 
+# IMPORT Mark's CUSTOM python module...
+from . import customFunctions as cf
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    
+    # RETRIEVE the request's queryString parameters... 
     name = req.params.get('name')
+    val1 = int(req.params.get('val1'))
+    val2 = int(req.params.get('val2'))
+
+    # ADD the 2 numbers... 
+    tempSum = cf.addNumbers(val1, val2)
+
+    # LOG to the live-streaming logging engine...
+    print("HEY - this message is in live-streaming logging", val1, val2, tempSum)
+    
     if not name:
         try:
             req_body = req.get_json()
@@ -16,7 +29,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. Marks first HTTP-triggered Azure Function executed successfully!")
+        return func.HttpResponse(f"Hello, {name}. The SUM of {val1} and {val2} is {tempSum}. Marks first HTTP-triggered Azure Function executed successfully!")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
